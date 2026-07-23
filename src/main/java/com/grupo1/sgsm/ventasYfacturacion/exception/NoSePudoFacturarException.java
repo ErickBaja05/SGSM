7c30618fb6a1537f7b29f0e10e69b00e9430a260
Erick Bajaña <erick.bajana@epn.edu.ec>
2026-07-23 00:53:40 -0500

    facturacionGYE lista

---
+++ src/main/java/com/grupo1/sgsm/ventasYfacturacion/exception/NoSePudoFacturarException.java
@@ -0,0 +1,7 @@
+package com.grupo1.sgsm.ventasYfacturacion.exception;
+
+public class NoSePudoFacturarException extends RuntimeException {
+    public NoSePudoFacturarException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/grupo1/sgsm/ventasYfacturacion/service/FacturasGYEServiceImpl.java
+++ src/main/java/com/grupo1/sgsm/ventasYfacturacion/service/FacturasGYEServiceImpl.java
@@ -2,12 +2,15 @@ package com.grupo1.sgsm.ventasYfacturacion.service;
 
 import com.grupo1.sgsm.inventarioYproductos.service.IProductoService;
 import com.grupo1.sgsm.inventarioYproductos.service.ProductoService;
+import com.grupo1.sgsm.ventasYfacturacion.dao.DetalleFacturaDAO;
 import com.grupo1.sgsm.ventasYfacturacion.dao.FacturaGyeDAO;
 import com.grupo1.sgsm.ventasYfacturacion.dto.DetalleFacturaDTO;
 import com.grupo1.sgsm.ventasYfacturacion.dto.FacturaGYEConsultadaDTO;
 import com.grupo1.sgsm.ventasYfacturacion.dto.InfoProductoDTO;
 import com.grupo1.sgsm.ventasYfacturacion.dto.NuevaFacturaDTO;
 import com.grupo1.sgsm.ventasYfacturacion.exception.FechasNoValidasException;
+import com.grupo1.sgsm.ventasYfacturacion.exception.NoSePudoFacturarException;
+import com.grupo1.sgsm.ventasYfacturacion.model.DetalleFactura;
 import com.grupo1.sgsm.ventasYfacturacion.model.FacturaGYE;
 
 import java.time.LocalDate;
@@ -18,6 +21,7 @@ public class FacturasGYEServiceImpl implements IFacturasGYEService {
 
     private FacturaGyeDAO facturaGyeDAO = new FacturaGyeDAO();
     private IProductoService productoService = new ProductoService();
+    private DetalleFacturaDAO detalleFacturaDAO = new DetalleFacturaDAO();
 
     @Override
     public List<FacturaGYEConsultadaDTO> consultarFacturas(LocalDate fechaInicio, LocalDate fechaFin) {
@@ -36,10 +40,43 @@ public class FacturasGYEServiceImpl implements IFacturasGYEService {
     @Override
     public void facturarProductos(NuevaFacturaDTO nuevaFacturaDTO, List<DetalleFacturaDTO> detallesFacturaDTO) {
 
+        try{
+            FacturaGYE facturaGYE = new FacturaGYE();
+            facturaGYE.setNumero_factura(nuevaFacturaDTO.getNumero_factura());
+            facturaGYE.setFecha_emision(nuevaFacturaDTO.getFecha_emision());
+            facturaGYE.setIVA(nuevaFacturaDTO.getIVA());
+            facturaGYE.setFecha_emision(nuevaFacturaDTO.getFecha_emision());
+            facturaGYE.setCodigo_sucursal(nuevaFacturaDTO.getCodigo_sucursal());
+            facturaGYE.setSubtotal(nuevaFacturaDTO.getSubtotal());
+            facturaGYE.setTotal(nuevaFacturaDTO.getTotal());
+            facturaGYE.setCedula_ciudadania(nuevaFacturaDTO.getCedula_ciudadania());
+
+            facturaGyeDAO.insertar(facturaGYE);
+
+            for (DetalleFacturaDTO detalleFacturaDTO : detallesFacturaDTO) {
+                DetalleFactura detalle = new DetalleFactura();
+                detalle.setCodigo_producto(detalleFacturaDTO.getCodigo_producto());
+                detalle.setCantidad(detalleFacturaDTO.getCantidad());
+                detalle.setPrecio_unitario(detalleFacturaDTO.getPrecio_unitario());
+                detalle.setNumero_factura(facturaGYE.getNumero_factura());
+                detalle.setCodigo_sucursal(facturaGYE.getCodigo_sucursal());
+                detalle.setSubtotal_producto(detalleFacturaDTO.getSubtotal_producto());
+                detalleFacturaDAO.insertar(detalle);
+
+            }
+        }catch (Exception e){
+            throw new NoSePudoFacturarException("Error desconocido, no se pudo facturar");
+        }
+
     }
 
     @Override
     public InfoProductoDTO agregarProductoCarrito(String codigoProducto) {
+        // LINEA DEL SERVICE DE PRODUCTO PARA CONSULTAR UN STOCK
+        // InfoProductoDTO info = productoService.consultarStockProducto(codigoProducto)
+        return new InfoProductoDTO("10","de prueba");
+
+
     }
 
 }
